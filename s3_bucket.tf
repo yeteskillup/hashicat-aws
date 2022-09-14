b87d1f96c01db241b238fe5eefcede8f39accf2b
Terry Ye <yeteskilup@gmail.com>
2022-09-15 01:22:12 +1000

    adding s3 bucket

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,11 +1,19 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket_prefix = "${var.prefix}"
-  bucket = "my-s3-bucket"
+#   bucket_prefix = "${var.prefix}"
+#   bucket = "my-s3-bucket"
+#   acl    = "private"
+
+#   versioning = {
+#     enabled = true
+#   }
+# }
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
   acl    = "private"
 
-  versioning = {
+  versioning {
     enabled = true
   }
 }
