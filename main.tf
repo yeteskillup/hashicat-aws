d2dbd9cd5854c4bb83b58c832e501d7bf27642d4
Terry Ye <yeteskilup@gmail.com>
2022-09-14 22:23:27 +1000

    Adding billable tags for sentinal policy check

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department= "DevOps"
+    Department = "DevOps"
+    Billable = true
   }
 }
 
